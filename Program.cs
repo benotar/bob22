660f9dc53df4356db658bad2bc26863c51f2193d
Ivan <zhmur.ivan@gmail.com>
2024-02-05 23:34:04 +0200

    adasd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿using System.IO;
+﻿using System;
 using System.Threading.Tasks;
 using System.Threading;
-using System;
 using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
@@ -15,11 +14,7 @@ namespace BotGiveInThe
     {
         private static async Task Main(string[] args)
         {
-           
-
             var botClient = new TelegramBotClient("6745861939:AAFGKY6IS9nr-Z9nW6HX4HSaNu7hhK1RPsc");
-
-            //var botClient = new TelegramBotClient(token!);
 
             using CancellationTokenSource cts = new();
 
@@ -33,51 +28,54 @@ namespace BotGiveInThe
                 pollingErrorHandler: HandlePollingErrorAsync,
                 receiverOptions: receiverOptions,
                 cancellationToken: cts.Token
-                );
+            );
 
             var me = await botClient.GetMeAsync();
 
             Console.WriteLine($"Start listening for @{me.Username}");
 
-            Console.ReadLine();
+            // Закоментуйте або видаліть Console.ReadLine();
 
+            //cts.Cancel(); // Не закривайте токен, доки програма активна
 
-            cts.Cancel();
+            // Очікування завершення програми
+            await Task.Delay(Timeout.Infinite);
+        }
 
-            async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is not { } message)
             {
-                if (update.Message is not { } message)
-                {
-                    return;
-                }
-
-                if (message.Text is not { } messageText)
-                {
-                    return;
-                }
-
-                var chatId = message.Chat.Id;
-
-                Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
-
-                Message sentMessage = await botClient.SendTextMessageAsync(
-                    chatId: chatId,
-                    text: "You said:\n" + messageText,
-                    cancellationToken: cancellationToken);
+                return;
             }
 
-            Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+            if (message.Text is not { } messageText)
             {
-                var ErrorMessage = exception switch
-                {
-                    ApiRequestException apiRequestException
+                return;
+            }
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            // Відповідь на повідомлення
+            await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        private static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
                     => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
+                _ => exception.ToString()
+            };
 
-                Console.WriteLine(ErrorMessage);
-                return Task.CompletedTask;
-            }
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
         }
     }
 }
